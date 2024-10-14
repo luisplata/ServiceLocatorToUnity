af6e0f9b9a26458599a8d9196d54273601d2d0bd
Luis Plata <www.luisplata@gmail.com>
2024-10-14 13:00:36 -0500

    improuve package name and adding more info to develop

--- Runtime/ServiceLocator.cs
+++ Runtime/ServiceLocator.cs
@@ -2,7 +2,7 @@ using System;
 using System.Collections.Generic;
 using UnityEngine.Assertions;
 
-namespace Plugins.ServiceLocator
+namespace BellsebossStudio.Plugins.ServiceLocator
 {
     /// <summary>
     /// Recomendations:
