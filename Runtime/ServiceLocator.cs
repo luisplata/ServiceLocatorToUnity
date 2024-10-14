b39f7dc255822fe3093bd78f0088cd44f3897019
Luis Plata <www.luisplata@gmail.com>
2024-10-14 16:06:44 -0500

    update serviceLocator

--- Runtime/ServiceLocator.cs
+++ Runtime/ServiceLocator.cs
@@ -2,7 +2,7 @@ using System;
 using System.Collections.Generic;
 using UnityEngine.Assertions;
 
-namespace BellsebossStudio.Plugins.ServiceLocator
+namespace BellsebossStudio.Plugins.ServiceLocatorPattern
 {
     /// <summary>
     /// Recomendations:
@@ -21,13 +21,12 @@ namespace BellsebossStudio.Plugins.ServiceLocator
             _services = new Dictionary<Type, object>();
         }
 
-        public void RegisterService<T>(T service)
+        public bool RegisterService<T>(T service)
         {
             var type = typeof(T);
-            Assert.IsFalse(_services.ContainsKey(type), 
+            Assert.IsFalse(_services.ContainsKey(type),
                 $"Service {type} already registered");
-        
-            _services.Add(type, service);
+            return _services.TryAdd(type, service);
         }
 
         public T GetService<T>()
@@ -38,9 +37,9 @@ namespace BellsebossStudio.Plugins.ServiceLocator
                 throw new Exception($"Service {type} not found");
             }
 
-            return (T) service;
+            return (T)service;
         }
-        
+
         public void UnregisterService<T>()
         {
             var type = typeof(T);
@@ -56,6 +55,5 @@ namespace BellsebossStudio.Plugins.ServiceLocator
         {
             _services.Clear();
         }
-    
     }
 }
